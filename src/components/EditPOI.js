f2ad5c4a803c82d88da978c09b5c7c5d8ab1b88d
Christian Bryant <christian.bryant@mobelux.com>
2016-12-10 07:44:08 -0700

    working on edit form

--- src/components/EditPOI.js
+++ src/components/EditPOI.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
 class EditPOI extends React.Component{
+
+  componentDidMount(){
+    //const s = document.getElementById('name').value;
+    
+
+  }
+
   render(){
     return(
-      <div>Edit POI Component</div>
+      <div>
+        <div className="admin-panel avenir">
+          <div className="admin-panel__header">Edit New Point of Interest</div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section">
+              <label>Name</label>
+              <input  id="name" type="text"  />
+            </div>
+          </div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section">
+              <label>Latitude</label>
+              <input id="latitude" type="text" value={this.props.latitude} />
+            </div>
+            <div className="admin-panel__section">
+              <label>Longitude</label>
+              <input id="longitude" type="text" value={this.props.longitude} />
+            </div>
+          </div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section">
+              <label>Short Description</label>
+              <textarea id="description" type="text" value={this.props.description} />
+            </div>
+          </div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section">
+              <label>Image</label>
+              <img className="edit-img" src={this.props.image}/>
+            </div>
+
+          </div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section">
+              <input id="image" type="file" />
+            </div>
+          </div>
+          <div className="admin-panel__form">
+            <div className="admin-panel__section admin-panel__section--center">
+              <button id="btnEditLocation" onClick={()=> this.props.editPointofInterest()} className="home-page__navbuttons--button admin-panel__button avenir">Save</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
--- src/containers/EditPOIContainer.js
+++ src/containers/EditPOIContainer.js
@@ -4,22 +4,47 @@ import base from '../base';
 
 class EditPOIContainer extends React.Component{
 
+  state = {
+    id: null,
+    name: null,
+    description: null,
+    latitude: null,
+    longitude: null,
+    image: null
+  }
+
   componentWillMount(){
+
     const editID = this.context.location.query.editId;
     base.fetch(`/pointofinterest/${editID}`, {
       context: this,
       asArray: false,
       then(data){
         console.log(data);
+
+        this.setState({
+          id: editID,
+          name: data.name,
+          description: data.description,
+          image: data.image,
+          latitude: data.latitude,
+          longitude: data.longitude,
+        })
+
       }
     });
   }
 
+  editPointofInterest(){
+    const name = document.getElementById('name').value;
+    alert(name);
+  }
+
   render(){
     return (
-      <div className="app-container">
-        <div>
-          <EditPOI  />
+      <div className="component-wrapper">
+        <div className="wrapper">
+          <EditPOI editPointofInterest={this.editPointofInterest} {...this.state}  />
         </div>
       </div>
     )
