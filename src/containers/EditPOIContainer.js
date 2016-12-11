c7bcee65faa94c8ee38bc7f56fa24b6c58db9ae5
Christian Bryant <christian.bryant@mobelux.com>
2016-12-11 11:10:43 -0700

    fixes saving issue

--- src/containers/EditPOIContainer.js
+++ src/containers/EditPOIContainer.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import EditPOI from '../components/EditPOI';
+
 import base from '../base';
 
 class EditPOIContainer extends React.Component{
 
-  state = {
-    id: null,
-    name: null,
-    description: null,
-    latitude: null,
-    longitude: null,
-    image: null
-  }
+  constructor(props) {
+   super(props);
+   this.state = {
+     id: '',
+     name: '',
+     description: '',
+     latitude: '',
+     longitude: '',
+     image: ''
+   }
 
-  componentWillMount(){
+   this.handleChange = this.handleChange.bind(this);
+   this.editPointofInterest = this.editPointofInterest.bind(this);
+
+ }
+
+  componentWillMount() {
 
     const editID = this.context.location.query.editId;
     base.fetch(`/pointofinterest/${editID}`, {
       context: this,
       asArray: false,
       then(data){
-        console.log(data);
 
+        console.log(data);
         this.setState({
           id: editID,
           name: data.name,
@@ -35,16 +42,123 @@ class EditPOIContainer extends React.Component{
     });
   }
 
-  editPointofInterest(){
-    const name = document.getElementById('name').value;
-    alert(name);
+  handleChange(event) {
+   switch (event.target.name) {
+     case 'name':
+       this.setState({ name: event.target.value });
+       break;
+    case 'longitude':
+       this.setState({ longitude: event.target.value })
+       break;
+    case 'latitude':
+       this.setState({ latitude: event.target.value })
+       break;
+    case 'description':
+        this.setState({ description: event.target.value })
+        break;
+     default:
+   }
+
   }
 
-  render(){
+  editPointofInterest(){
+
+    let id = this.context.location.query.editId;
+
+
+    const name = this.state.name;
+    const latitude = this.state.latitude;
+    
+
+
+    document.getElementById('btnEditLocation').innerHTML = 'Saving...';
+    const image = document.getElementById('image').files;
+    if ( image.length > 0 ){
+
+      const storageRef = base.storage().ref();
+      let file = image[0];
+      var metadata = {
+        'contentType': file.type
+      }
+
+      storageRef.child(`images/${file.name}`).put(file,metadata).then(function(snapshot){
+
+        const url = snapshot.metadata.downloadURLs[0];
+        base.update(`pointofinterest/${id}`, {
+          data: { name: this.state.name, image: url, latitude: this.state.latitude, longitude: this.state.longitude, description: this.state.description },
+          then(err){
+            if(!err){
+                document.getElementById('btnEditLocation').innerHTML = 'Save';
+            }
+          }
+        })
+
+      }).catch(function(error){
+        console.log("Upload Failed: ", error);
+      })
+
+    } else {
+
+      base.update(`pointofinterest/${id}`, {
+        data: { name: this.state.name, latitude: this.state.latitude, longitude: this.state.longitude, description: this.state.description },
+        then(err){
+          if(!err){
+              document.getElementById('btnEditLocation').innerHTML = 'Save';
+          }
+        }
+      })
+
+    }
+
+
+
+  }
+
+  render() {
     return (
       <div className="component-wrapper">
         <div className="wrapper">
-          <EditPOI editPointofInterest={this.editPointofInterest} {...this.state}  />
+          <div className="admin-panel avenir">
+            <div className="admin-panel__header">Edit Point of Interest</div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section">
+                <label>Name</label>
+                <input name="name" type="text" value={this.state.name} onChange={this.handleChange}   />
+              </div>
+            </div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section">
+                <label>Latitude</label>
+                <input name="latitude" type="text" value={this.state.latitude} onChange={this.handleChange}  />
+              </div>
+              <div className="admin-panel__section">
+                <label>Longitude</label>
+                <input name="longitude" type="text" value={this.state.longitude} onChange={this.handleChange}  />
+              </div>
+            </div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section">
+                <label>Short Description</label>
+                <textarea name="description" type="text" value={this.state.description} onChange={this.handleChange}  />
+              </div>
+            </div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section">
+                <label>Image</label>
+                <img className="edit-img" src={this.state.image}/>
+              </div>
+            </div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section">
+                <input id="image" type="file" />
+              </div>
+            </div>
+            <div className="admin-panel__form">
+              <div className="admin-panel__section admin-panel__section--center">
+                <button id="btnEditLocation" onClick={this.editPointofInterest} className="home-page__navbuttons--button admin-panel__button avenir">Save</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
